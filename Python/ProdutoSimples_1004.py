343fe7d1d4ee32da0ff7316525b8f1552fe7be5e
Gustavo Rocha <87160095+MyNameIsGustavo@users.noreply.github.com>
2022-09-29 22:22:18 -0300

    Add files via upload

---
+++ Python/ProdutoSimples_1004.py
@@ -0,0 +1,13 @@
+'''	Desenvolvedor: @Gustavo Rocha.
+	Tecnologia utilizada: Python.
+	Nome do exercício: Produto simples. 
+	ID: 1004
+	Status da submissão: ACEITA.
+'''
+
+n1 = int(input())
+n2 = int(input())
+
+produto = n1 * n2
+
+print("PROD = %d" %produto)

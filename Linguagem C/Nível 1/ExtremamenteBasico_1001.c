7b049cfac4752e247fbad4b9c23c906e6b1a2a24
Gustavo Rocha <87160095+MyNameIsGustavo@users.noreply.github.com>
2022-09-29 22:12:53 -0300

    Add files via upload

---
+++ Linguagem C/Nível 1/ExtremamenteBasico_1001.c
@@ -0,0 +1,21 @@
+/*	Desenvolvedor: @Gustavo Rocha.
+	Tecnologia utilizada: C.
+	Nome do exercício: Extremamente básico. 
+	ID: 1001
+	Status da submissão: ACEITA.
+*/
+
+#include <stdio.h>
+
+int main (void)
+{	int A, B;
+	int X;
+
+	scanf("%d %d", &A, &B);
+	
+	X = A + B;
+	
+	printf("X = %d\n", X);
+	
+	return 0;
+}

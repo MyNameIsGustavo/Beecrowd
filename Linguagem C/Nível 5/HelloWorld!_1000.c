efeec734316c508651e4d6232e4f6e01682173c9
Gustavo Rocha <87160095+MyNameIsGustavo@users.noreply.github.com>
2022-10-04 20:40:45 -0300

    Add files via upload

---
+++ Linguagem C/Nível 5/HelloWorld!_1000.c
@@ -0,0 +1,14 @@
+/*	Desenvolvedor: @Gustavo Rocha.
+	Tecnologia utilizada: C.
+	Nome do exercício: Hello World! 
+	ID: 1000
+	Status da submissão: ACEITA.
+*/
+
+#include <stdio.h>
+
+int main (void)
+{	printf("Hello World!\n");
+
+	return 0;
+}

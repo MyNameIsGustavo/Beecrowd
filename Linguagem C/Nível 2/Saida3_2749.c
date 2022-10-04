efafdc00446384ba0f749de3c2922d230f34f73e
Gustavo Rocha <87160095+MyNameIsGustavo@users.noreply.github.com>
2022-10-04 20:19:46 -0300

    Add files via upload

---
+++ Linguagem C/Nível 2/Saida3_2749.c
@@ -0,0 +1,21 @@
+/*	Desenvolvedor: @Gustavo Rocha.
+	Tecnologia utilizada: C.
+	Nome do exercício: Saída 3. 
+	ID: 2749
+	Status da submissão: ACEITA.
+*/
+
+#include <stdio.h>
+
+int main()
+{
+    printf("---------------------------------------\n");
+    printf("|x = 35                               |\n");
+    printf("|                                     |\n");
+    printf("|               x = 35                |\n");
+    printf("|                                     |\n");
+    printf("|                               x = 35|\n");
+    printf("---------------------------------------\n");
+    
+    return 0;
+}

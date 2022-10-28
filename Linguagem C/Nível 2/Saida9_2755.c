7ad2ad92624dced8454d68338d15b0270c1a83ef
Gustavo Rocha <87160095+MyNameIsGustavo@users.noreply.github.com>
2022-10-28 20:05:05 -0300

    Add files via upload

---
+++ Linguagem C/Nível 2/Saida9_2755.c
@@ -0,0 +1,20 @@
+/*	Desenvolvedor: @Gustavo Rocha.
+	Tecnologia utilizada: C.
+	Nome do exercício: Saída 9. 
+	ID: 2755
+	Status da submissão: ACEITA.
+*/
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main(void) 
+{   printf("\"Ro'b'er\tto\\/\"\n");
+    printf("(._.) ( l: ) ( .-. ) ( :l ) (._.)\n");
+    printf("(^_-) (-_-) (-_^)\n");
+    printf("(\"_\") (\'.\')\n");
+    
+    return 0;
+}

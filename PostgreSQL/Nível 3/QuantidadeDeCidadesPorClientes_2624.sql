9a32f57664949e2f424e97955162161317a52a85
Gustavo Rocha <87160095+MyNameIsGustavo@users.noreply.github.com>
2022-10-28 20:07:52 -0300

    Update QuantidadeDeCidadesPorClientes_2624.sql

--- PostgreSQL/Nível 3/QuantidadeDeCidadesPorClientes_2624.sql
+++ PostgreSQL/Nível 3/QuantidadeDeCidadesPorClientes_2624.sql
@@ -1,6 +1,6 @@
 /*	Desenvolvedor: @Gustavo Rocha.
 	Tecnologia utilizada: PostgreSQL.
-	Nome do exercício: Quantidade de Cidades por Clients. 
+	Nome do exercício: Quantidade de Cidades por Clientes. 
 	ID: 2624
 	Status da submissão: ACEITA.
 */

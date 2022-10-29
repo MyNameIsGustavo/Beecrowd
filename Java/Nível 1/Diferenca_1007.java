e0f33a793b483a65c0cb79b16ab2c3a5d79de440
Gustavo Rocha <87160095+MyNameIsGustavo@users.noreply.github.com>
2022-10-29 10:59:48 -0300

    Add files via upload

---
+++ Java/Nível 1/Diferenca_1007.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main 
+{   public static void main(String[] args) 
+    {   Scanner input = new Scanner(System.in);
+        
+        int A = input.nextInt();
+        int B = input.nextInt();
+        int C = input.nextInt();
+        int D = input.nextInt();
+        int DIFERENCA = (A * B - C * D);
+        
+        System.out.println("DIFERENCA = " + DIFERENCA);
+        
+        input.close();
+    }
+}
